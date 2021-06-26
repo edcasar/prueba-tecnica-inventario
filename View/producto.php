88f6efa82117a9b27f37f3bfc227f42b06b0d5ba
Edcas804 <harmonygtr@gmail.com>
2021-06-26 14:39:50 -0500

    Se crea la estructura basica

---
+++ View/producto.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="description" content="Prueba técnica php" />
+	<link rel="stylesheet" href="../assets/styles/main.css" />
+	<title>Inventarios de productos</title>
+  </head>
+  <body>
+  </body>
+</html>
